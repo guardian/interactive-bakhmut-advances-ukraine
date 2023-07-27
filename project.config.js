1de9df804f1b7d39d7bccd552b5c157adb00ffc6
7091lapS <antonio.voce@guardian.co.uk>
2023-07-27 15:47:53 +0100

    style header furniture

--- project.config.js
+++ project.config.js
@@ -2,8 +2,8 @@ export default {
     title: "Interactive Ukraine advances in Bakhmut",
     path: "2022/06/interactive-ukraine-advances-in-bakhmut",
     placeholders: {
-        headline: "Headline goes here",
-        standfirst: "Don't forget the standfirst",
+        headline: "A year of war: how Russian forces have been pushed back in Ukraine",
+        standfirst: "Guardian analysis shows how Russia has lost a fifth of the land it once controlled in Ukraine after its invasion",
         paragraphBefore: "",
     },
     // Array of atoms to be excluded from build and deploy
