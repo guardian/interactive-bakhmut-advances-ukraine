5ebbd5a3ba5231d860b8af91cc9812df52a4d769
7091lapS <antonio.voce@guardian.co.uk>
2023-07-31 10:29:28 +0100

    Better scrolly style

--- src/atoms/default/app.prerender.js
+++ src/atoms/default/app.prerender.js
@@ -1,21 +1,22 @@
 import request from "request-promise"
-import fs from "fs"
 
 export async function render() {
 
-	
+	const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct','Nov','Dec']
 
 	const doc = await request({"uri":'https://interactive.guim.co.uk/docsdata-test/1Dabx4Lqs0ZgecD4Xo2TUib7G-N9XkuDEhBcaHkIDCaE.json', json:true});
 
     let html = ``
     doc.chapters.forEach(element => {
 
+        const dateArr = element.date.split('/')
+        const date = `${dateArr[0]} ${months[parseInt(dateArr[1])]} ${dateArr[2]}`
 
         if(element.chapter)
         {
             html += `<div class="scroll-text__inner">
                 <div class="scroll-text__div">
-                    <time>${element.date || ''}</time>
+                    <time>${element.date ? date : ''}</time>
                     <h2>${element.headingText}</h2>
                     <p>${element.bodyText}</p>
                 </div>
