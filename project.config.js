26e800275d6f4471910e4b1e68d9e9645156f952
Seán Clarke <sean.clarke@guardian.co.uk>
2024-07-11 15:58:00 +0100

    changing config before attempting chasiv yar version

--- project.config.js
+++ project.config.js
@@ -1,6 +1,6 @@
 export default {
     title: "Interactive Ukraine advances in Bakhmut",
-    path: "2022/06/interactive-ukraine-advances-in-bakhmut",
+    path: "2024/07/interactive-ukraine-advances-in-bakhmut",
     placeholders: {
         headline: "L year of war: how Russian forces have been pushed back in Ukraine",
         standfirst: "Guardian analysis shows how Russia has lost a fifth of the land it once controlled in Ukraine after its invasion",
